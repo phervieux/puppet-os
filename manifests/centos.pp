474392d37cb0b3725aadcf8445f62aff5f92932f
Marc Fournier <marc.fournier@camptocamp.com>
2012-10-24 10:58:14 +0200

    os: update augeas resource to current practices.

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,17 +1,18 @@
 class os::centos {
- 
+
   # set hostname
-  host {"$fqdn":
+  host {$::fqdn:
     ensure => present,
-    ip => $ipaddress,
+    ip => $::ipaddress,
   }
   augeas {"set hostname":
-    context => "/files/etc/sysconfig/network",
-    changes => "set HOSTNAME $fqdn",
+    incl    => '/etc/sysconfig/network',
+    lens    => 'Sysconfig.lns',
+    changes => "set HOSTNAME ${::fqdn}",
     notify  => Exec["set hostname"],
   }
   exec {"set hostname":
-    command     => "hostname $fqdn",
-    unless      => "hostname -f | grep -q $fqdn",
+    command     => "hostname ${::fqdn}",
+    unless      => "hostname -f | grep -q ${::fqdn}",
   }
 }
