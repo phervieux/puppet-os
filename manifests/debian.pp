3147e76b850c6ffa13cd166d1d3954f3c5fa281c
Mickaël Canévet <mickael.canevet@camptocamp.com>
2013-10-09 12:22:36 +0200

    Use shellvar instead of template to manage /etc/adduser.conf

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -3,49 +3,64 @@ class os::debian {
   # Default packages
   #
   package {
-    "at":             ensure => present; # usefull for reboots...
-    "bc":             ensure => present;
-    "bash-completion":ensure => present;
-    "bzip2":          ensure => present;
-    "cadaver":        ensure => present;
-    "cron-apt":       ensure => purged; # Keeps a fresh apt database
-    "emacs21-common": ensure => absent;
-    "gettext":        ensure => present;
-    "iproute":        ensure => present;
-    "locate":         ensure => absent;
-    "lynx":           ensure => present;
-    "xfsprogs":       ensure => present;
-  }
-  
-  # Umask, etc.
-  file { "/etc/profile":
-    ensure => present,
-    mode   => 644,
-    source => "puppet:///modules/os/etc/profile-debian",
+    'at':             ensure => present; # usefull for reboots...
+    'bc':             ensure => present;
+    'bash-completion':ensure => present;
+    'bzip2':          ensure => present;
+    'cadaver':        ensure => present;
+    'cron-apt':       ensure => purged; # Keeps a fresh apt database
+    'emacs21-common': ensure => absent;
+    'gettext':        ensure => present;
+    'iproute':        ensure => present;
+    'locate':         ensure => absent;
+    'lynx':           ensure => present;
+    'xfsprogs':       ensure => present;
   }
 
-  file {"/etc/profile.d":
+  # Umask, etc.
+  file { '/etc/profile':
+    ensure => present,
+    mode   => '0644',
+    source => 'puppet:///modules/os/etc/profile-debian',
+  }
+
+  file {'/etc/profile.d':
     ensure => directory
   }
 
   # Kernel
-  file { "/etc/modules":
+  file { '/etc/modules':
     ensure => present,
   }
 
-  file {"/etc/adduser.conf":
-    ensure => present,
-    owner => root,
-    group => root,
-    mode => 644,
-    content => template("os/etc/adduser.conf.erb"),
+  file {'/etc/adduser.conf':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+  }
+
+  if $::adduser_first_uid != undef {
+    shellvar { 'FIRST_UID':
+      ensure => present,
+      target => '/etc/adduser.conf',
+      value  => $::adduser_first_uid,
+    }
+  }
+
+  if $::adduser_first_gid != undef {
+    shellvar { 'FIRST_GID':
+      ensure => present,
+      target => '/etc/adduser.conf',
+      value  => $::adduser_first_gid,
+    }
   }
 
   # $LANG
-  file { "/etc/environment":
+  file { '/etc/environment':
     ensure => present,
-    mode   => 644,
-    source => "puppet:///modules/os/etc/environment",
+    mode   => '0644',
+    source => 'puppet:///modules/os/etc/environment',
     owner  => root,
     group  => root,
   }
