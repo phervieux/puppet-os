2bc612b7439d7d1c0973ce94a6d0a1c4b59b2c14
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-04-25 15:43:08 +0200

    Remove stuffs moved to accounts::config

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -33,29 +33,6 @@ class os::debian {
     ensure => present,
   }
 
-  file {'/etc/adduser.conf':
-    ensure  => present,
-    owner   => root,
-    group   => root,
-    mode    => '0644',
-  }
-
-  if $::adduser_first_uid != undef {
-    shellvar { 'FIRST_UID':
-      ensure => present,
-      target => '/etc/adduser.conf',
-      value  => $::adduser_first_uid,
-    }
-  }
-
-  if $::adduser_first_gid != undef {
-    shellvar { 'FIRST_GID':
-      ensure => present,
-      target => '/etc/adduser.conf',
-      value  => $::adduser_first_gid,
-    }
-  }
-
   # $LANG
   file { '/etc/environment':
     ensure => present,
